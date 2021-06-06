26e097fe0761a211f1a9026457fda6460c0288c5
Grzegorz <grzegorzsobieraj@interia.pl>
2021-06-06 14:53:14 +0200

    Update useful_functions.py
    Added another two functions. Minor corrections to existing functions.

--- useful_functions.py
+++ useful_functions.py
@@ -1,5 +1,6 @@
+
 def calculate_pit(income):
-    """Get income and return tax due."""
+    """Get income and return Polish due tax."""
     if type(income) not in (int, float):
         raise TypeError
     relief = 0
@@ -22,8 +23,26 @@ def calculate_pit(income):
     return round(tax, 2)
 
 
+def liters_100km_to_miles_gallon(liters_per_100km):
+    """Convert liters per 100 km (l/100km) to miles per US gallon (mpg)."""
+    if type(liters_per_100km) != float:
+        raise TypeError
+    if liters_per_100km <= 0:
+        raise ValueError
+    return 3.785411784 * 100 / (1.609344 * liters_per_100km)
+
+
+def miles_gallon_to_liters_100km(miles_per_gallon):
+    """Convert miles per US gallon (mpg) to liters per 100 km (l/100km)."""
+    if type(miles_per_gallon) != float:
+        raise TypeError
+    if miles_per_gallon <= 0:
+        raise ValueError
+    return 100*3.785411784/(1.609344 * miles_per_gallon)
+
+
 def right_triangle_hypotenuse(leg_a, leg_b):
-    """Get lenghts of both legs of right triangle and return lenght of hypotenuse."""
+    """Get lenghts of both legs of right triangle and return lenght of the hypotenuse."""
     if type(leg_a) not in (int, float) or type(leg_b) not in (int, float):
         raise TypeError
     if leg_a <= 0 or leg_b <= 0:
